595363bedee986b9295fa1528b17c59049e2a512
andres.montaño <andres.montano@babelgroup.com>
2024-04-25 13:14:53 +0200

    -Clase coche

--- src/main/java/es/babel/Modelos/Coche.java
+++ src/main/java/es/babel/Modelos/Coche.java
@@ -1,4 +1,18 @@
 package es.babel.Modelos;
 
 public class Coche {
+    private double peso;
+
+    public Coche(){}
+    public Coche(double peso){
+        this.peso = peso;
+    }
+    
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
 }
