416212f5fca3631345ba7e1fce1cdd97a2ec58ee
maría.sisamon <maria.sisamon@babelgroup.com>
2024-04-25 13:38:38 +0200

    puente model

--- src/main/java/es/babel/Modelos/Puente.java
+++ src/main/java/es/babel/Modelos/Puente.java
@@ -2,11 +2,13 @@ package es.babel.Modelos;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Puente {
     List<Coche> listaCoches;
     double peso;
