c67463892a006b03c5078bc8580050b0addfe720
maría.sisamon <maria.sisamon@babelgroup.com>
2024-04-25 13:22:09 +0200

    create car service

--- src/main/java/es/babel/Main.java
+++ src/main/java/es/babel/Main.java
@@ -4,8 +4,6 @@ import es.babel.Application.PuenteApp;
 import es.babel.Config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
--- src/main/java/es/babel/Servicios/CocheServicio.java
+++ src/main/java/es/babel/Servicios/CocheServicio.java
@@ -1,8 +1,15 @@
 package es.babel.Servicios;
 
+import es.babel.Modelos.Coche;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CocheServicio {
+    public Coche crearNuevoCoche () {
+        return new Coche(asignarPeso());
+    }
 
+    public double asignarPeso() {
+        return Math.random() * (2000 - 800) + 800;
+    }
 }
