9f839a2608962c4d8dab06604f2a2bc5472e67b2
maría.sisamon <maria.sisamon@babelgroup.com>
2024-04-25 13:29:53 +0200

    create car service

--- src/main/java/es/babel/Application/PuenteApp.java
+++ src/main/java/es/babel/Application/PuenteApp.java
@@ -1,11 +1,19 @@
 package es.babel.Application;
 
+import es.babel.Servicios.ICocheServicio;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PuenteApp{
 
+    private final ICocheServicio cocheServicio;
+
+    public PuenteApp(ICocheServicio cocheServicio) {
+        this.cocheServicio = cocheServicio;
+    }
+
     public void run() {
 
     }
+
 }
